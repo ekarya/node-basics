9e547799d2950704471f0c0238cc4ae7cda723a6
Verdi Pratama <verdipratama@icloud.com>
2019-02-27 11:28:09 +0700

    Making simple brooo!

--- 01-hello-world/server.js
+++ 01-hello-world/server.js
@@ -1,6 +1,5 @@
 // import http module & port
 const http = require("http");
-const port = 3000;
 
 function cobaPanggil(req, res) {
     res.writeHead(200, { "Content-Type": "text/plain" });
@@ -8,9 +7,4 @@ function cobaPanggil(req, res) {
     res.end();
 }
 
-http.createServer(cobaPanggil).listen(port, () => {
-    console.log("Server running bro please check localhost:3000");
-});
-
-// Simple createServer
-// http.createServer(cobaPanggil).listen(3000);
+http.createServer(cobaPanggil).listen(3000);
