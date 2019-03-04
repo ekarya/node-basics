e309f7f9a3616874f1f7fdf31e71a9d88ad5d4d0
Verdi Pratama <verdipratama@icloud.com>
2019-03-04 20:02:54 +0700

    + Tambah keterangan +

--- 04-node-routing/server.js
+++ 04-node-routing/server.js
@@ -2,6 +2,8 @@ const port = 3000;
 const http = require("http");
 const app = require("./app");
 
+// app = dari namafile yang dipanggil
+// routingURL = nama function module.exports
 http.createServer(app.routingURL).listen(port, () => {
     console.log("Server running bro please check localhost:3000");
 });
